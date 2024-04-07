0cf83ad5bf1303afc8f9dad2b976d754195881b8
carolinenewcom <carolinenewcom33@gmail.com>
2024-04-07 09:44:56 -0500

    caseload page layout

--- caseload/views.py
+++ caseload/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def caseload(request):
+    return render(request, 'caseload/dashboard.html')
+
