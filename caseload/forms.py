156ad4eb8399857478fa16b6bccc910f021c5fd5
carolinenewcom <carolinenewcom33@gmail.com>
2024-04-25 23:01:28 -0500

    added styling to forms

--- caseload/forms.py
+++ caseload/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Child, Schedule, Session
 
 from django.utils.translation import gettext_lazy as _
@@ -12,11 +12,52 @@ class AddChild(ModelForm):
     class Meta:
         model = Child
         fields = '__all__'
+        labels = {
+            'id_number': '',
+            'first_name': '',
+            'last_name': '',
+            'user': '',
+
+        }
+        widgets = {
+            'id_number': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Child ID'
+                }),
+            'first_name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'First Name'
+                }),
+            'last_name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Last Name'
+                })
+        }
 
 class AddSchedule(ModelForm):
     class Meta:
         model = Schedule
         fields = '__all__'
+        labels = {
+            'scheduled_week_day': '',
+            'session_time': '',
+            'child': '',
+        }
+        widgets = {
+            'scheduled_week_day': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Scheduled Day'
+                }),
+            'session_time': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Session Time 00:00'
+                })
+        }
 
 class NewSessionLog(ModelForm):
     class Meta:
